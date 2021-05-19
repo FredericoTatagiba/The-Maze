d55c938de15f596fbb1fb8b353b4743c2470d282
Frederico Ferreira Tatagiba <fredericotatagiba@gmail.com>
2021-05-19 17:47:32 -0300

    Correção e adicionado novos elementos

--- Assets/Scripts/MoveAround.cs
+++ Assets/Scripts/MoveAround.cs
@@ -8,23 +8,19 @@ public class MoveAround : MonoBehaviour
 
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        //Variaveis para receber os valores de movimentação do player
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         float rotation = Input.GetAxis("LookAround");
 
-        //Calculo de movimentação de acordo com o transform e as posições
         Vector3 moveCal = transform.right * x + transform.forward * z;
 
-        //Depois esse calculo é aplicado ao controller de movimento do player
-        controller.Move(moveCal * 10 * Time.deltaTime);
+        controller.Move(moveCal * 100 * Time.deltaTime);
 
-        //Em seguida é aplicado a variavel de rotação da camera
-        GetComponent<Transform>().Rotate(Vector3.up * rotation * 10);
+        GetComponent<Transform>().Rotate(Vector3.up * rotation * 50);
     }
 }
